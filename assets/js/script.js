faf2c3c3c54382eafdfff88a3dd6d4568ae6b6dc
Elaine Deng <qd9069@gmail.com>
2022-04-10 16:44:39 -0700

    Updated code to modify the UI page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,4 @@
-// set a var to store API Key
-var APIKey = "019e9eae1c058887e25d507a189a7e6f";
-
-// Create Variables for the API Call - input city
-var city;
-
-//URL example of how to make an API call using just the city name
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-// Make the API Call Using Fetch to get the weather info
-// fetch(queryURL)
-
-// to get user input city
-var userInput = document.getElementById("input");
-var searchButton = document.getElementById("searchBtn");
-
-
-//--------------- for local storage ----------------
-// searchButton.addEventListener("click", function(event) {
-    // event.preventDefault();
-            
-    // get input value from search form
-    // var inputCity = userInput.value.trim();
-           
-    // use setItem to store the input text value in localStorage
-    // localStorage.setItem("event", inputCity);
-                
-// });
-
-// get the input text value from local storage
-// var savedCity = localStorage.getItem("event");
-// console.log(savedCity)
-
-// to keep the saved city persists
-// if (savedCity) {
-//     userInput.value = savedCity;
-// }
-
-
-
-
-// Make the Geocoding API Call Using Fetch to get city lat and lon
-function getCity(event) { 
-    event.preventDefault();
-
-    var inputCity = userInput.value.trim();
-    // console.log(inputCity);
-
-    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&limit=1&appid=" + APIKey;
-  
-    fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // console.log(data);
-
-        var cityLat = data[0].lat
-        var cityLon = data[0].lon
-        // console.log(cityLat);
-        // console.log(cityLon);
-
-    });
-
-}
-
-searchButton.addEventListener("click", getCity);
-// ******* how to get the inputCity, cityLon and cityLat outside of the function? **********
-
-var cityLat = "32.7174202";
-var cityLon = "-117.1627728";
+// set all vars for weather info
 var currentTemp = document.getElementById("temp");
 var currentWind = document.getElementById("wind");
 var currentHumidity = document.getElementById("humidity");
@@ -113,56 +42,122 @@ var date4Icon = document.getElementById("date4Icon");
 var date5Icon = document.getElementById("date5Icon");
 var datesIcon = [date1Icon, date2Icon, date3Icon, date4Icon, date5Icon];
 
-// Make the oneCall API Call Using Fetch to get weather info
-function getWeather() {
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + APIKey;
-    var inputCity = "San Diego"
 
+// set a var to store API Key
+var APIKey = "019e9eae1c058887e25d507a189a7e6f";
+
+// to get user input city
+var userInput = document.getElementById("input");
+var searchButton = document.getElementById("searchBtn");
+
+// to hide the window info before clicking search button
+var right = document.getElementById("right");
+right.style.display = "none";
+
+//--------------- for local storage ----------------
+// searchButton.addEventListener("click", function(event) {
+    // event.preventDefault();
+            
+    // get input value from search form
+    // var inputCity = userInput.value.trim();
+           
+    // use setItem to store the input text value in localStorage
+    // localStorage.setItem("event", inputCity);
+                
+// });
+
+// get the input text value from local storage
+// var savedCity = localStorage.getItem("event");
+// console.log(savedCity)
+
+// to keep the saved city persists
+// if (savedCity) {
+//     userInput.value = savedCity;
+// }
+//----------------------------------------------------
+
+
+// Make the Geocoding API Call Using Fetch to get city lat and lon
+function getCity(event) { 
+    event.preventDefault();
+
+    // to show the window info after clicking the search button
+    right.style.display = "block";
+
+    var inputCity = userInput.value.trim();
+    // console.log(inputCity);
+
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&limit=1&appid=" + APIKey;
+  
     fetch(requestUrl)
-      .then(function (response) {
+    .then(function (response) {
         return response.json();
-      })
-      .then(function (data) {
+    })
+    .then(function (data) {
         console.log(data);
 
-        // to show weather in the main weather box
-        var icon = document.getElementById("icon");
-        var addIcon = icon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
+        var cityLat = data[0].lat
+        var cityLon = data[0].lon
+        // console.log(cityLat);
+        // console.log(cityLon);
+        getWeather();
 
-        currentCityDate.textContent = inputCity + " (" + currentDay + ")";
-        currentTemp.textContent = data.current.temp + "°F";
-        currentWind.textContent = data.current.wind_speed + " MPH";
-        currentHumidity.textContent = data.current.humidity + " %";
+        // Make the oneCall API Call Using Fetch to get weather info
+        function getWeather() {
+            var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + APIKey;
+            // to print the inputCity as upper case
+            function capitalizeFirstLetter(inputCity) {
+                return inputCity.charAt(0).toUpperCase() + inputCity.slice(1);
+              }
+            var printInputCity = capitalizeFirstLetter(inputCity);
         
-        // to color the UV index
-        if (data.current.uvi < 3) {
-            currentUV.classList.add("low");
-        } 
-        else if (3 <= data.current.uvi && data.current.uvi < 6) {
-            currentUV.classList.add("moderate");
-        } 
-        else {
-            currentUV.classList.add("high");
-        }
+            fetch(queryURL)
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (data) {
+                // console.log(data);
         
-        currentUV.textContent = data.current.uvi;
-
-        // to show weather in  the forecastBlock array [1] for the tomorrow ... [5] for 5 days later
-        for (var i=1; i < 6; i++) {
-            dates[i-1].textContent = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
-            datesTemp[i-1].textContent = "Temp: " + data.daily[i].temp.day + "°F";
-            datesWind[i-1].textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
-            datesHum[i-1].textContent = "Humidity: " + data.daily[i].humidity + " %";
-            var addDatesIcon = datesIcon[i-1].setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+                // to show weather in the main weather box
+                var icon = document.getElementById("icon");
+                var addIcon = icon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
+        
+                currentCityDate.textContent = printInputCity + " (" + currentDay + ")";
+                currentTemp.textContent = data.current.temp + "°F";
+                currentWind.textContent = data.current.wind_speed + " MPH";
+                currentHumidity.textContent = data.current.humidity + " %";
+                
+                // to color the UV index
+                if (data.current.uvi < 3) {
+                    currentUV.classList.add("low");
+                } 
+                else if (3 <= data.current.uvi && data.current.uvi < 6) {
+                    currentUV.classList.add("moderate");
+                } 
+                else {
+                    currentUV.classList.add("high");
+                }
+                
+                currentUV.textContent = data.current.uvi;
+        
+                // to show weather in  the forecastBlock array [1] for the tomorrow ... [5] for 5 days later
+                for (var i=1; i < 6; i++) {
+                    dates[i-1].textContent = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
+                    datesTemp[i-1].textContent = "Temp: " + data.daily[i].temp.day + "°F";
+                    datesWind[i-1].textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
+                    datesHum[i-1].textContent = "Humidity: " + data.daily[i].humidity + " %";
+                    var addDatesIcon = datesIcon[i-1].setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+                }
+        
+              });
         }
 
-      });
+
+    });
+
 }
-getWeather();
 
-
-
-
+searchButton.addEventListener("click", getCity);
 
 
 // example for Geocoding API
