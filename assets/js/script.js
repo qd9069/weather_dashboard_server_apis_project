6ca3431b5c10a77897293bea1785b7de2f98ea0c
Elaine Deng <qd9069@gmail.com>
2022-04-11 02:16:47 -0700

    Modified fetch functions to use for city history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,6 @@ var date4Icon = document.getElementById("date4Icon");
 var date5Icon = document.getElementById("date5Icon");
 var datesIcon = [date1Icon, date2Icon, date3Icon, date4Icon, date5Icon];
 
-
 // set a var to store API Key
 var APIKey = "019e9eae1c058887e25d507a189a7e6f";
 
@@ -55,17 +54,13 @@ var right = document.getElementById("right");
 right.style.display = "none";
 
 
+// --------- for Fetch function ------------------
 // Make the Geocoding API Call Using Fetch to get city lat and lon
-function getCity(event) { 
-    event.preventDefault();
+function getCity(cityName) { 
+    cityName = cityName.trim();
+    // console.log(cityName);
 
-    // to show the window info after clicking the search button
-    right.style.display = "block";
-
-    var inputCity = userInput.value.trim();
-    // console.log(inputCity);
-
-    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&limit=1&appid=" + APIKey;
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + APIKey;
   
     fetch(requestUrl)
     .then(function (response) {
@@ -74,76 +69,77 @@ function getCity(event) {
     .then(function (data) {
         console.log(data);
 
-        var cityLat = data[0].lat
-        var cityLon = data[0].lon
+        var cityLat = data[0].lat;
+        var cityLon = data[0].lon;
         // console.log(cityLat);
         // console.log(cityLon);
-        getWeather();
+       //  getWeather();
+
+        getWeather(cityName, cityLat, cityLon);
 
         // Make the oneCall API Call Using Fetch to get weather info
-        function getWeather() {
-            var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + APIKey;
-            // to print the inputCity as upper case
-            function capitalizeFirstLetter(inputCity) {
-                return inputCity.charAt(0).toUpperCase() + inputCity.slice(1);
-              }
-            var printInputCity = capitalizeFirstLetter(inputCity);
-        
-            fetch(queryURL)
-              .then(function (response) {
-                return response.json();
-              })
-              .then(function (data) {
-                // console.log(data);
-        
-                // to show weather in the main weather box
-                var icon = document.getElementById("icon");
-                var addIcon = icon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
-        
-                currentCityDate.textContent = printInputCity + " (" + currentDay + ")";
-                currentTemp.textContent = data.current.temp + "°F";
-                currentWind.textContent = data.current.wind_speed + " MPH";
-                currentHumidity.textContent = data.current.humidity + " %";
-                
-                // to color the UV index
-                if (data.current.uvi < 3) {
-                    currentUV.classList.add("low");
-                } 
-                else if (3 <= data.current.uvi && data.current.uvi < 6) {
-                    currentUV.classList.add("moderate");
-                } 
-                else {
-                    currentUV.classList.add("high");
-                }
-                
-                currentUV.textContent = data.current.uvi;
-        
-                // to show weather in  the forecastBlock array [1] for the tomorrow ... [5] for 5 days later
-                for (var i=1; i < 6; i++) {
-                    dates[i-1].textContent = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
-                    datesTemp[i-1].textContent = "Temp: " + data.daily[i].temp.day + "°F";
-                    datesWind[i-1].textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
-                    datesHum[i-1].textContent = "Humidity: " + data.daily[i].humidity + " %";
-                    var addDatesIcon = datesIcon[i-1].setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
-                }
-        
-              });
-        }
-
 
     });
-
 }
 
-searchButton.addEventListener("click", getCity);
+// function to get weather info
+function getWeather(cityName, cityLat, cityLon) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + APIKey;
+    // to print the inputCity as upper case
+
+    var printInputCity = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // to show weather in the main weather box
+        var icon = document.getElementById("icon");
+        var addIcon = icon.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
+
+        currentCityDate.textContent = printInputCity + " (" + currentDay + ")";
+        currentTemp.textContent = data.current.temp + "°F";
+        currentWind.textContent = data.current.wind_speed + " MPH";
+        currentHumidity.textContent = data.current.humidity + " %";
+        
+        // to color the UV index
+        if (data.current.uvi < 3) {
+            currentUV.classList.add("low");
+        } 
+        else if (3 <= data.current.uvi && data.current.uvi < 6) {
+            currentUV.classList.add("moderate");
+        } 
+        else {
+            currentUV.classList.add("high");
+        }
+        
+        currentUV.textContent = data.current.uvi;
+
+        // to show weather in  the forecastBlock array [1] for the tomorrow ... [5] for 5 days later
+        for (var i=1; i < 6; i++) {
+            dates[i-1].textContent = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
+            datesTemp[i-1].textContent = "Temp: " + data.daily[i].temp.day + "°F";
+            datesWind[i-1].textContent = "Wind: " + data.daily[i].wind_speed + " MPH";
+            datesHum[i-1].textContent = "Humidity: " + data.daily[i].humidity + " %";
+            var addDatesIcon = datesIcon[i-1].setAttribute("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png");
+        }
+
+      });
+}
+
 
 
 //--------------- for local storage ----------------
 var cityHistory = [];
 
 searchButton.addEventListener("click", function(event) {
+
     event.preventDefault();
-            
+        
+    // to show the window info after clicking the search button
+    right.style.display = "block";
+
     // get input value from search form
     var inputCity = userInput.value.trim();
            
@@ -159,6 +155,7 @@ searchButton.addEventListener("click", function(event) {
     // store value: stringify and set key in localStorage to cityHistroy array
     localStorage.setItem("city", JSON.stringify(cityHistory));
 
+    getCity(inputCity);
     renderCityHistory();
                 
 });
@@ -179,6 +176,14 @@ function renderCityHistory () {
         button.classList.add("btn-block");
         button.classList.add("btn-secondary");
         button.classList.add("btn");
+        button.classList.add("btnHistory");
+        button.addEventListener("click", function(event) {
+           // event.preventDefault();
+
+            right.style.display = "block";
+
+            getCity(this.textContent);
+        });
 
         cityList.appendChild(button);
         
@@ -200,13 +205,11 @@ function init() {
   }
 
 // Calls init to retrieve data and render it to the page on load
-init()
-
-//----------------------------------------------------
-
-// searchButton.addEventListener("click", getCity);
+init();
 
 
+
+// ------- example ---------------------
 // example for Geocoding API
 // function geoLocationApiExample() { 
 //     var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=San Diego&limit=5&appid=" + APIKey;
